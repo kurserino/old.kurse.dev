c0c9fe60486be4191298aea3db2fb56f5db4df8d
Kurse <lucas.kurse@hotmail.com>
2021-04-28 19:56:08 -0300

    Header icons css update

--- src/Header/Icons.js
+++ src/Header/Icons.js
@@ -1,29 +1,33 @@
 import React from "react";
-import styled from "@emotion/styled";
+import { css, jsx } from "@emotion/react";
 import Link from "./Link";
 import InstagramIcon from "../Icons/Instagram";
 import GithubIcon from "../Icons/Github";
 import EmailIcon from "../Icons/Email";
 
-var Icons = ({ className, spacing }) => (
-  <div className={className}>
+var Icons = ({ ...props }) => (
+  <div
+    css={css`
+      display: flex;
+      justify-content: space-between;
+      margin-left: auto;
+      margin-bottom: 10px;
+
+      @media screen and (min-width: 991px) {
+        margin-right: 15px;
+      }
+    `}
+  >
     <Link href="mailto:lucas.kurse@hotmail.com" target="_blank">
-      <EmailIcon width={20}/>
+      <EmailIcon width={20} />
     </Link>
     <Link href="https://www.instagram.com/kuurse/" target="_blank">
-      <InstagramIcon width={20}/>
+      <InstagramIcon width={20} />
     </Link>
     <Link href="https://github.com/iamkrs" target="_blank">
-      <GithubIcon width={20}/>
+      <GithubIcon width={20} />
     </Link>
   </div>
 );
 
-var StyledIcons = styled(Icons)`
-  display: flex;
-  justify-content: space-between;
-  margin-left: auto;
-  margin-bottom: 10px;
-`;
-
-export default StyledIcons;
+export default Icons;
