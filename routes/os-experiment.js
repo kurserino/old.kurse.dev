45ca2b5e47c0a7a72c0e3365748a6747baa325a0
Kurse <lucas.kurse@hotmail.com>
2021-04-30 15:17:17 -0300

    Server updated

--- routes/os-experiment.js
+++ routes/os-experiment.js
@@ -1,4 +1,14 @@
-const app = express.Router();
 const express = require("express");
+const app = express();
 const axios = require("axios");
 const path = require("path");
+const fs = require("fs");
+
+const router = express.Router();
+router.use("/os-experiment", express.static(path.join(__dirname, "../public/os-experiment")));
+
+// router.get("/os-experiment", function (req, res) {
+//   res.sendFile(path.join(__dirname, '../public/os-experiment/index.html'));
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require("path");
 const app = express();
 const api = express.Router();
 const fs = require("fs");
-const https = require('https');
-const http = require('http');
+const https = require("https");
+const http = require("http");
 require("dotenv").config();
 
 const mongoose = require("mongoose");
@@ -23,40 +23,23 @@ app.use(express.static(path.join(__dirname, "build")));
 
 // Additional routes
 const routes = ["/api"];
-routesFiles = fs.readdirSync(path.join(__dirname, "/routes"));
-
+routesFiles = fs.readdirSync(path.join(__dirname, "routes"));
 routesFiles.forEach((file) => {
   var route = file.replace(".js", "");
-  var routeFolder = path.join(__dirname, "public", route);
-  if (fs.existsSync(routeFolder)) {
-    app.use("/" + route, express.static(routeFolder));
-  }
-  routes.push(route);
+  app.use(require(path.join(__dirname, "routes", file)));
+  routes.push("/" + route);
 });
 
 app.get("*", function (req, res, next) {
   routes.forEach((route) => {
     if (req.url.startsWith(route)) return next();
   });
-  res.sendFile(
-    path.join(
-      __dirname,
-      process.env.NODE_ENV == "development" ? "public" : "build",
-      "index.html"
-    )
-  );
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 // Running message
 api.get("/", function (req, res) {
   res.json({ message: "Api running." });
-});
-
-// Get illustrations from instagram
-api.get("/illustrations", async (req, res) => {
-  const { IG_USER, IG_PASSWORD } = process.env;
-
-  res.json({ message: "Nozes" });
 });
 
 app.use("/api", api);
@@ -71,7 +54,7 @@ if (process.env.NODE_ENV == "development") {
     },
     app
   );
-  
+
   httpsServer.listen(443, () => {
     console.log("HTTPS Server running on port 443");
   });
